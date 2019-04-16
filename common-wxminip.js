ee67e22d04f872450e40e4db786854f9d6ce3c6d
18047096 <18047096@cnsuning.com>
2019-04-16 20:26:20 +0800

    增加wx请求

--- common-wxminip.js
+++ common-wxminip.js
@@ -2,6 +2,9 @@ module.exports = {
     getEntry: getEntry, //获取json节点  
     isObjectNotEmpty: isObjectNotEmpty, // 对象是否为非空
     isNotNil: isNotNil, // 判断某个节点是否为空
+    get: Get,
+	post: Post,
+	upload: Upload
 }
 
 /**
@@ -108,4 +111,59 @@ function isPhoneX(back) {
         }
     })
 }
+
+function Get(url, data, cb) {
+	wx.request({
+		method: 'GET',
+		url: url,
+		data: data,
+		success: (res) => {
+			typeof cb == "function" && cb(res.data, "");
+		},
+		fail: (err) => {
+			typeof cb == "function" && cb({}, err.errMsg);
+			console.log("出错了", url);
+		},
+		complete: () => {
+		}
+	});
+};
+
+function Post(url, data, cb) {
+	wx.request({
+		method: 'POST',
+		url: url,
+		data: data,
+		success: (res) => {
+			typeof cb == "function" && cb(res.data, "");
+		},
+		fail: (err) => {
+			typeof cb == "function" && cb({}, err.errMsg);
+			// console.log("http请求:"+config.HTTP_url);
+			console.log(err)
+			console.log("出错了", url);
+		},
+		complete: () => {
+		}
+	});
+};
+
+function Upload(url, file, data, cb) {
+	wx.uploadFile({
+		url: url,
+		filePath: file,
+		name: "file",
+		formData: data,
+		success: (res) => {
+			if (typeof (res.data) == "string") {
+				typeof cb == "function" && cb(JSON.parse(res.data), "");
+			} else {
+				typeof cb == "function" && cb(res.data, "");
+			}
+		},
+		fail: (err) => {
+			typeof cb == "function" && cb(null, err.errMsg);
+		}
+	});
+};
 
